ba878b924f50b8141249549315a72b72bacfb2eb
Thundercat <michael.c.haines@gmail.com>
2015-06-15 19:31:33 -0600

    pretty much done?

--- tiered-pricing.js
+++ tiered-pricing.js
@@ -29,6 +29,22 @@ Items = new Mongo.Collection('items');
 if (Meteor.isClient) {
   Session.setDefault('style', 'ORU0004');
 
+  var logHello = function(){
+    console.log('hello');
+  };
+
+  var currentTriggerValues = function(){
+    var t = [];
+    var triggerThresholds = $('.triggerRow').find('.qty-sold-trigger');
+    var salePercents = $('.triggerRow').find('.percent-off');
+    for (var i=0; i<triggerThresholds.length; i++){
+      t.push({'qtySold': triggerThresholds[i].value, 'salePercent': salePercents[i].value});
+    }
+    console.log(t);
+    return t;
+
+  };
+
   Template.tieredPricing.events({
     "click #add-item": function(){
       console.log('adding item');
@@ -56,11 +72,27 @@ if (Meteor.isClient) {
       console.log(i);
     },
 
+    /*
+    "change .qty-sold-trigger": function(e){
+      logHello();
+      currentTriggerValues();
+      //Items.update(Session.get('selectedItemTriggersId'), {$set: {: ! this.checked}});
+      Items.update(Session.get('selectedItemTriggersId'), {$set: {qtySoldTriggers: currentTriggerValues()}});
+    },
+    */
+
+    "click #add-row": function(){
+      //var newQtySoldTrigger = document.getElementById('new-trigger-qty-sold').value;
+      //var newSalePercent = document.getElementById('new-trigger-sale-percent').value;
+      Items.update(Session.get('selectedItemTriggersId'), {$push: {qtySoldTriggers: {'qtySold': null, 'salePercent': null}}});
+    },
+
+    "click #save-btn": function(){
+      Items.update(Session.get('selectedItemTriggersId'), {$set: {qtySoldTriggers: currentTriggerValues()}});
+    },
+
     "submit #set-style": function(e){
       var s = document.getElementById('input-style').value.toUpperCase();
-      console.log('Trying to get item');
-      console.log(s);
-
       Meteor.http.get('http://api.backcountry.com/v1/products/' + s + '?fields=skus', function (err, res) {
         console.log(res.statusCode, res.data);
         if(res.statusCode == 200){
@@ -75,16 +107,10 @@ if (Meteor.isClient) {
 
 
 Template.tieredPricing.helpers({
-  items: function(){
-    return Items.find({})
-  },
-
   colors: function(){
     var colors = [];
     var selectedItem = Session.get('selectedItem');
-    console.log(selectedItem);
     if(typeof(selectedItem) != 'undefined'){
-      console.log('pushing colors');
       selectedItem.products[0].skus.forEach(function(sku){
         color = sku.color.name;
         if (colors.indexOf(color) == -1){
@@ -96,29 +122,23 @@ Template.tieredPricing.helpers({
   },
 
   triggers: function(){
-    console.log('finding triggers for current item');
-
     var selectedItem = Session.get('selectedItem');
-    console.log(selectedItem);
     if (typeof(selectedItem) == 'undefined'){
       return
     }
     var selectedItemTriggers = Items.find({'style': selectedItem.products[0].id}).fetch()[0];
 
-    console.log(selectedItem);
-    console.log(selectedItemTriggers);
     if (typeof(selectedItemTriggers) == 'undefined'){
-      console.log('giving up and returning');
-      return false
+      Items.insert({
+        'style': selectedItem.products[0].id,
+        'qtySoldTriggers': []
+      });
+      selectedItemTriggers = Items.find({'style': selectedItem.products[0].id}).fetch()[0];
     }
 
-
-    console.log(selectedItemTriggers);
-    console.log(selectedItemTriggers.qtySoldTriggers);
+    Session.set('selectedItemTriggersId', Items.find({'style': selectedItem.products[0].id}).fetch()[0]._id);
     return selectedItemTriggers.qtySoldTriggers
-
   }
-
 });
 
 };
